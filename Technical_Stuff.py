a607d25140ab8d209a33477341f37ef99421f517
Oswald Steinhelß <101434848+RezerdPrime@users.noreply.github.com>
2023-08-03 14:29:34 +1000

    Update Technical_Stuff.py

--- Technical_Stuff.py
+++ Technical_Stuff.py
@@ -4,7 +4,9 @@ from random import randint
 from math import *
 from clipboard import paste
 from os import system
+from datetime import datetime
 from sys import exc_info
+
 
 # WINDOW INIT ==========================================================================================================
 
@@ -13,8 +15,8 @@ config = configparser.ConfigParser()
 config.read("config.cfg", encoding="utf-8")
 
 WINDOW_TITLE = list(map(lambda x: x.strip(),
-                        config['Splashes']['splshs'].split(";")))
-# theme = bool(int(config['Settings']['is_dark']))
+                    config['Splashes']['splshs'].split(";")))
+#theme = bool(int(config['Settings']['is_dark']))
 SPLASHES = WINDOW_TITLE[randint(0, len(WINDOW_TITLE) - 1)]
 pg.display.set_caption(SPLASHES)
 font40 = pg.font.Font("fnt.otf", 40)
@@ -25,6 +27,7 @@ WIDTH = int(config['Settings']['width'])
 HEIGHT = int(config['Settings']['height'])
 WINDOW_SIZE = (WIDTH, HEIGHT)
 player_count = int(ceil((WIDTH * HEIGHT / (WIDTH + HEIGHT)) ** 0.3))
+
 
 # SOME COLORS ==========================================================================================================
 
@@ -41,6 +44,7 @@ LIGHT_BLUE = (0, 255, 255)
 BLUE = (0, 0, 255)
 PURPLE = (225, 0, 255)
 
+
 # VARIABLES FOR MODES MANAGING =========================================================================================
 
 screen = pg.display.set_mode(WINDOW_SIZE)
@@ -48,11 +52,11 @@ screen.fill(WHITE)
 overlay = pg.Surface((WIDTH, HEIGHT))
 overlay.set_alpha(0)
 
-mode = "menu"  # menu game editor settings endscreen
+mode = "menu" # menu game editor settings endscreen
 running = True
 mouse_pos = (0, 0)
 
-func_speed = 3
+func_speed = 2
 fs_list = [0.25, 0.5, 0.75, 1, 2, 4, 8]
 
 
@@ -63,17 +67,16 @@ class Circle:
         self.color = color
         self.pos = pos
         self.radius = radius
-    
-    def draw(self, x, y, sc=screen):
-        pg.draw.circle(sc, self.color,
-                       (self.pos[0] + x, self.pos[1] + y), self.radius)
+
+    def draw(self, x, y, sc = screen):
+        pg.draw.circle(sc, self.color, (self.pos[0] + x, self.pos[1] + y), self.radius)
 
 
 class Line:
     def __init__(self, startpos, endpos):
         self.startpos = startpos
         self.endpos = endpos
-    
+
     def draw(self, x, y):
         pg.draw.line(screen, BLACK,
                      (self.startpos[0] + x, self.startpos[1] + y),
@@ -83,49 +86,46 @@ class Line:
 class Map:
     def __init__(self):
         self.obj_list = []
-    
+
     def add(self, *objs):
         self.obj_list.extend(objs)
-    
+
     def __getitem__(self, item):
         return self.obj_list[item]
-    
+
     def __iter__(self):
         return iter(self.obj_list)
-    
+
     def generate(self):
-        for i in range(
-            HEIGHT * WIDTH // 1000):  # '''HEIGHT * WIDTH // 500'''
-            self.add(Circle(
-                (randint(150, 255), randint(150, 255),
-                 randint(150, 255)),
-                (dx + randint(-2 * WIDTH, WIDTH),
-                 dy + randint(-2 * HEIGHT, HEIGHT)), randint(10, 50)))
-    
-    def draw_all(self, x, y, sc=screen):
+        for i in range(HEIGHT * WIDTH // 1000): #'''HEIGHT * WIDTH // 500'''
+            self.add( Circle(
+                (randint(150,255), randint(150,255),randint(150,255)),
+            (dx + randint(-2 * WIDTH, WIDTH), dy + randint(-2 * HEIGHT, HEIGHT)), randint(10,50)))
+
+    def draw_all(self, x, y, sc = screen):
         for i in range(len(self.obj_list)):
             self.obj_list[i].draw(x, y)
 
 
 class Player:
-    
-    def __init__(self, pos, team: pg.Color, kills=0):
+
+    def __init__(self, pos, team : pg.Color, indx):
         self.pos = pos
         self.team = team
-        self.kills = kills
-    
+        self.indx = indx
+
     def draw(self, x, y):
-        pg.draw.circle(screen, self.team,
-                       (self.pos[0] + x, self.pos[1] + y), 20)
-    
-    def attack(self, x, y, func, func_dir, fs=func_speed):
+        pg.draw.circle(screen, self.team, (self.pos[0] + x, self.pos[1] + y), 20)
+
+    def attack(self, x, y, func, func_dir, fs = func_speed):
         val = func_dir;
-        cur = 0; dcur= 0
+        cur = 0;
+        dcur = 0
         indx = len(Mappy.obj_list)
         avaliable_colors[0] = self.team
         # buff = -eval(func.replace("x", str(val / 20)))
         buff = evaluated_value(func, val)
-        
+
         # print(buff, lasterr)
         if isinstance(buff, str):
             while ((isinstance(buff, str) and
@@ -133,16 +133,16 @@ class Player:
                    and abs(val) < 1000):
                 val += func_dir
                 buff = evaluated_value(func, val)
-            
+
             buff = 0
             if val == 1000:
                 val = func_dir * 21
                 draw_map(x - val, y - cur, func, 0, fs, True)
-        
+
         if not isinstance(buff, str):
             dcur = buff
             buff = 0
-        
+
         while is_not_collided():
             pg.time.delay(round(4 / fs_list[fs]))
             val += func_dir
@@ -150,61 +150,58 @@ class Player:
             if isinstance(cur, str):
                 cur = buff
                 break
-            
+
             cur -= dcur
             print(cur - buff)
             if abs(val) <= 5 and abs(buff - cur) > 1:
                 continue
-            
+
             Mappy.add(Line((val - func_dir - x + WIDTH // 2,
                             buff - y + HEIGHT // 2),
                            (val - x + WIDTH // 2,
                             cur - y + HEIGHT // 2)))
             draw_map(x - val, y - cur, func, 0, fs, True)
             buff = cur
-        
+
         Mappy.obj_list = Mappy.obj_list[:indx]
         detonated_pos = (val - x + WIDTH // 2,
                          cur - y + HEIGHT // 2)
-        
-        kill_players(detonated_pos)
-        self.kills += kill_players(detonated_pos)
-        
+
+        Teams[0 if self.team == RED else 1].kill_counts[self.indx] \
+            += kill_players(detonated_pos)
+
         Mappy.add(Circle(WHITE, detonated_pos, 30))
-        
+
         draw_map(x - val, y - cur, func, 0, fs, True)
         pg.draw.circle(screen, WHITE, (WIDTH // 2, HEIGHT // 2), 30)
-        screen.blit(screen, (0, 0))
+        screen.blit(screen, (0,0))
         pg.display.flip()
         pg.time.delay(1500)
 
-
 class Team(Map):
-    kill_counts = [0 for _ in range(player_count)]
-    
-    def __init__(self, col: pg.Color):
+
+    def __init__(self, col : pg.Color):
         super().__init__()
         self.col = col
-    
+        self.kill_counts = [0 for _ in range(player_count)]
+
     def generate(self, mappy: Map):
         pc_buff = player_count
         newsc = pg.display.set_mode(WINDOW_SIZE)
-        # Map.draw_all()
-        
+        #Map.draw_all()
+
         while pc_buff:
-            gen_pos = (dx + randint(-2 * WIDTH, WIDTH),
-                       dy + randint(-2 * HEIGHT, HEIGHT))
+            gen_pos = (dx + randint(-2 * WIDTH, WIDTH), dy + randint(-2 * HEIGHT, HEIGHT))
             newsc.fill(WHITE)
             mappy.draw_all(dx - gen_pos[0], dy - gen_pos[1], newsc)
-            
-            flag_lst = [WHITE == newsc.get_at(
-                (dx + int(ceil(20 * cos(pi * k / 4))),
-                 dy + int(ceil(20 * sin(pi * k / 4))))
+
+            flag_lst = [ WHITE == newsc.get_at(
+                (dx + int(ceil(20 * cos(pi * k / 4))), dy + int(ceil(20 * sin(pi * k / 4))))
             ) for k in range(8)] + [WHITE == newsc.get_at((dx, dy))]
-            
+
             if all(flag_lst):
-                self.add(Player(gen_pos, self.col))
-                mappy.add(Player(gen_pos, self.col))
+                self.add(Player(gen_pos, self.col, player_count - pc_buff))
+                mappy.add(Player(gen_pos, self.col,  player_count - pc_buff))
                 pc_buff -= 1
 
 
@@ -213,7 +210,8 @@ class Team(Map):
 PLAY_color = (0, 0, 0)
 EDITOR_color = (0, 0, 0)
 SETTINGS_color = (0, 0, 0)
-pos_flag = -1  # показатель положения кнопки в меню
+pos_flag = -1 # показатель положения кнопки в меню
+
 
 # SETTINGS =============================================================================================================
 
@@ -231,18 +229,16 @@ FPS = fps_list.index(int(config['Settings']['fps']))
 # GAME =================================================================================================================
 
 overlay2 = pg.Surface((WIDTH, HEIGHT))
-overlay2.set_alpha(0)  # просто frontend
-choose = True  # выбор между загрузкой своей карты и рандомно сгенерированной
-NO_color = (0, 0, 0);
-YES_color = (255, 255, 255)
-YorN = 0  # флаг показывающий выбор игрока (чётен - No; нечётен - Yes)
+overlay2.set_alpha(0) # просто frontend
+choose = True # выбор между загрузкой своей карты и рандомно сгенерированной
+NO_color = (0,0,0); YES_color = (255,255,255)
+YorN = 0 # флаг показывающий выбор игрока (чётен - No; нечётен - Yes)
 
-dx = WIDTH // 2;
-dy = HEIGHT // 2  # Смещение координат (разница между текущим и предыдущим положениями мыши)
-prevmp = mouse_pos  # предыдущее положение мыши
+dx = WIDTH // 2; dy = HEIGHT // 2 # Смещение координат (разница между текущим и предыдущим положениями мыши)
+prevmp = mouse_pos # предыдущее положение мыши
 Mappy = Map()
-already_generated = False  # чтобы бесконечно не генерило карту
-mouse_mode = False  # применяется для скроллинга карты
+already_generated = False # чтобы бесконечно не генерило карту
+mouse_mode = False # применяется для скроллинга карты
 
 Teams = [Team(RED), Team(BLUE)]
 step = randint(0, 1)
@@ -251,6 +247,7 @@ func = ""
 func_font = pg.font.Font("fnt.otf", 30)
 avaliable_colors = [Teams[step % 2].col, WHITE, BLACK]
 func_dir = 1
+
 xxx = ("max", "exp", "frexp", "ldexp")
 fconvert = {"arcsin": "asin", "arccos": "acos", "arctg": "atan",
             "arcctg": "pi/2 - atan", "tg": "tan", "ctg": "1/tan",
@@ -260,6 +257,7 @@ you_god_damn_right = tuple(("abs ceil gcd lcm trunc log log2 "
                             "tau sum round for in range i j k t s u v "
                             "+ - ** * // / % , ( )").split())
 lasterr = ""
+
 
 # ENDSCREEN ============================================================================================================
 
@@ -289,15 +287,15 @@ def menu_interface(PLAY_color, EDITOR_color, SETTINGS_color):
               200, 80),
              ((WIDTH - 200) // 2, (HEIGHT + 130) // 2,
               200, 80)]
-    
+
     pg.draw.rect(screen, GRAY, butns[0])
     pg.draw.rect(screen, GRAY, butns[1])
     pg.draw.rect(screen, GRAY, butns[2])
-    
+
     pg.draw.rect(screen, LIGHT_GRAY, butns[3])
     pg.draw.rect(screen, LIGHT_GRAY, butns[4])
     pg.draw.rect(screen, LIGHT_GRAY, butns[5])
-    
+
     text1 = font40.render("PLAY", True, PLAY_color)
     screen.blit(text1, ((WIDTH - text1.get_width()) // 2,
                         (HEIGHT - 240) // 2))
@@ -312,54 +310,45 @@ def menu_interface(PLAY_color, EDITOR_color, SETTINGS_color):
 
 def load_data():
     system('explorer /select,"C:\\"')
-    
+
     while True:
         file_path = ""
         try:
-            while ".gpht" != file_path[-6:-1] or file_path.count(
-                ".gpht") != 1:
+            while ".gpht" != file_path[-6:-1] or file_path.count(".gpht") != 1:
                 file_path = paste()
-            
-            info = open(file_path.strip('"'), "r",
-                        encoding='utf-8').read()
+
+            info = open(file_path.strip('"'), "r", encoding='utf-8').read()
             map_data = []
-            
+
             exec("map_data += " + info)
             return map_data
-        
-        except SyntaxError:
-            continue
+
+        except SyntaxError: continue
 
 
-def convert_data(data: list):
+def convert_data(data : list):
     for tupl in data:
-        if len(tupl) == 6 and all(
-            [tupl[-1] >= 0] + [tupl[i] >= 0 for i in range(3)]) >= 0:
+        if len(tupl) == 6 and all([tupl[-1] >= 0] + [tupl[i] >= 0 for i in range(3)]) >= 0:
             Mappy.add(Circle((tupl[:3]), (tupl[3:-1]), tupl[-1]))
 
 
-def draw_map(x, y, func, speed_choice, fs=func_speed, flag=False):
+def draw_map(x, y, func, speed_choice, fs=func_speed, flag = False):
     screen.fill(WHITE)
     Mappy.draw_all(x, y)
     Teams[0].draw_all(x, y)
     Teams[1].draw_all(x, y)
-    
-    text_surface = func_font.render('f(x) = ' + func, True, BLACK)
+
+    text_surface = func_font.render('f(x) = ' + func, True, (0, 0, 0))
     screen.blit(text_surface, (50, HEIGHT - 68))
     pg.draw.rect(screen, BLACK, (30, HEIGHT - 80, WIDTH - 60, 60), 3)
-    
-    error = evaluated_value(func, 0)
-    if func and isinstance(error, str):
-        text_surface = font28.render(error, True, RED)
-        screen.blit(text_surface, (WIDTH - 400, HEIGHT - 68))
-    
+
     LEFTARROW = (BLACK,)
     RIGHTARROW = (BLACK,)
     if speed_choice == 1:
         LEFTARROW = (WHITE, LIGHT_BLUE)
     elif speed_choice == 2:
         RIGHTARROW = (WHITE, LIGHT_BLUE)
-    
+
     text_surface = font28.render('Speed:', True, BLACK)
     screen.blit(text_surface, (WIDTH - 140, 20))
     text_surface = font28.render(' < ', True, *LEFTARROW)
@@ -369,10 +358,10 @@ def draw_map(x, y, func, speed_choice, fs=func_speed, flag=False):
     screen.blit(text_surface, (WIDTH - 130, 60))
     text_surface = font28.render(' > ', True, *RIGHTARROW)
     screen.blit(text_surface, (WIDTH - 50, 60))
-    
+
     if flag:
         pg.draw.circle(screen, BLACK, (WIDTH // 2, HEIGHT // 2), 5)
-    
+
     pg.display.flip()
 
 
@@ -380,33 +369,50 @@ def is_not_collided():
     flag_lst = [screen.get_at((
         WIDTH // 2 + int(ceil(5 * cos(pi * k / 4))),
         HEIGHT // 2 + int(ceil(5 * sin(pi * k / 4)))
-    
+
     )) in avaliable_colors for k in range(8)] + \
-               [screen.get_at(
-                   (WIDTH // 2, HEIGHT // 2)) in avaliable_colors]
-    
+               [screen.get_at((WIDTH // 2, HEIGHT // 2)) in avaliable_colors]
+
     return all(flag_lst)
+
+
+def kill_players(det_pos):
+
+    kills = 0
+    dist = lambda P, Q: ((P[0] - Q[0])**2 + (P[1] - Q[1])**2)**.5
+
+    dist_lst = [[],[]]
+    dist_lst[0] = [dist(pl.pos, det_pos) for pl in Teams[0].obj_list]
+    dist_lst[1] = [dist(pl.pos, det_pos) for pl in Teams[1].obj_list]
+
+    for k in [0, 1]:
+        for i in range(len(dist_lst[k])):
+            if dist_lst[k][i] <= 50:
+                killed = Teams[k].obj_list[i]
+                Teams[k].obj_list.remove(killed)
+                kills += 1
+    return kills
 
 
 def evaluated_value(func: str, val):
     global lasterr
-    
+
     lasterr = ""
     for f in xxx:
         if f in func:
             func = func.replace(f, f[:f.index('x')] + " "
                                 + f[f.index('x') + 1:])
-    
+
     func = func.replace("x", str(val / 40))
     for f in xxx:
         fx = f[:f.index('x')] + " " + f[f.index('x') + 1:]
         func = func.replace(fx, f)
-    
+
     func = func.replace('^', '**')
-    
+
     for f in fconvert:
         func = func.replace(f, fconvert[f])
-    
+
     try:
         if '|' in func:
             if func.count('|') % 2 == 0:
@@ -415,17 +421,17 @@ def evaluated_value(func: str, val):
                 func = func.replace("|", ")")
             else:
                 raise SyntaxError("unmatched '|' (1 line)")
-        
+
         # Проверка на допустимые слова и символы
         fnc = func
         for x in (list(xxx) + list(fconvert.values())
                   + list(you_god_damn_right)):
             fnc = fnc.replace(x, " ")
-        
+
         for c in fnc:
             if c not in "01234567890. \t\r":
                 raise SyntaxError("Undefined syntax (1 line)")
-        
+
         return -int(40 * eval(func))
     except (ZeroDivisionError, NameError):
         lasterr = "ZeroDiv" if 'Zero' in str(exc_info()[0]) else "Name"
@@ -437,53 +443,11 @@ def evaluated_value(func: str, val):
         lasterr = "Type"
         if 'complex' in str(exc_info()[1]):
             return "Complex numbers are forbidden!"
-        
+
         return str(exc_info()[1])
     except ValueError:
         lasterr = "Value"
         return "Forbidden value!"
-
-
-# print(evaluated_value('-x', 1))  # 1
-# print(evaluated_value('1/x', 0))  # ZrDvE
-# print(evaluated_value('log(x, 1)', 80))  # ZrDvE
-# print(evaluated_value('log(1, x)', 0))  # VlE
-# print(evaluated_value('e^x', 1))  # TpE
-# print(evaluated_value('x**0.67', -1))  # 3
-# print(evaluated_value('0**x', 0))  # -40
-# print(evaluated_value('sin(x)**cos(x)', -pi / 3))  # TpE
-# print(evaluated_value('sin(x)**cos(x)', 100 * pi / 3))  # -72
-# print(evaluated_value('tgamma(x)', 1))  # NmE
-# print(evaluated_value('tan(x)', 20 * pi))  # -653249574127814784
-# print(evaluated_value('min(-x**2, 1)', 40))  # 40
-# print(evaluated_value('arcctg(x)', 0))  # VlE
-# print(evaluated_value('log(e, x)', -40))  # VlE
-# print(evaluated_value('(1/x)**-1', 0))  # ZrDvE
-# print(evaluated_value('gamma(x)', 0))  # VlE
-# print(evaluated_value('exp(x)', 2))  # SnE
-# print(evaluated_value('|ln(x)|', 80))
-# print(evaluated_value('', 1))  # SnE
-# print(evaluated_value('(-x)**1.75', 2))  # TpE
-# print(evaluated_value('"5" + ""', 1))
-# -5555555555555555555555555555555555555555
-
-
-def kill_players(det_pos):
-    kills = 0
-    dist = lambda P, Q: ((P[0] - Q[0]) ** 2 + (P[1] - Q[1]) ** 2) ** .5
-    
-    dist_lst = [[], []]
-    dist_lst[0] = [dist(pl.pos, det_pos) for pl in Teams[0].obj_list]
-    dist_lst[1] = [dist(pl.pos, det_pos) for pl in Teams[1].obj_list]
-    
-    for k in [0, 1]:
-        for i in range(len(dist_lst[k])):
-            if dist_lst[k][i] <= 50:
-                killed = Teams[k].obj_list[i]
-                Teams[k].kill_counts[i] += killed.kills
-                Teams[k].obj_list.remove(killed)
-                kills += 1
-    return kills
 
 
 '''def to_theme(*args, theme='light'):
